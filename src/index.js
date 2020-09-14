a44bc78c60139c4febbbbdf330af88d8fbdfbfe3
natapas <natapas.lue@gmail.com>
2020-09-14 20:47:24 +0700

    Add user to context

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import { ApolloServer, gql } from 'apollo-server';
+import mongoose from 'mongoose'
+
 import { typeDefs } from './graphql/typedefs';
 import { resolvers } from './graphql/resolvers';
+
+import './infra/mongoose'
+import UserRepository from './repositories/user_repository'
 
 const server = new ApolloServer({
   typeDefs: gql`
     ${typeDefs}
   `,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    const { userid } = req.headers
+
+    const userRepository = new UserRepository()
+    const user = await userRepository.findOne({
+      _id: mongoose.Types.ObjectId(userid)
+    })
+
+    return { user }
+  }
 });
 
 server.listen().then(({ url }) => {
