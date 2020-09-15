2dd8c7a5c450ece55f3d1d42ec0b05c838a5ab7d
natapas <natapas.lue@gmail.com>
2020-09-16 06:49:49 +0700

    Setup mongooes db

--- src/infra/mongoose.js
+++ src/infra/mongoose.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 
-mongoose.connect(process.env.DB_URL, {
+const DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/exam-backend'
+
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }, () => {
