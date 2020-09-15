563a66d49059785775813717260aa50ba549fcec
natapas <natapas.lue@gmail.com>
2020-09-15 12:45:55 +0700

    Update user resolver spec test case

--- __tests__/graphql/resolvers/user.spec.js
+++ __tests__/graphql/resolvers/user.spec.js
@@ -1,18 +1,5 @@
-import { createTestClient } from 'apollo-server-testing';
-import { ApolloServer, gql } from 'apollo-server';
-
-import { typeDefs } from '../../../src/graphql/typedefs';
-import { resolvers } from '../../../src/graphql/resolvers';
+import UserResolver from '../../../src/graphql/resolvers/user'
 import UserRepository from '../../../src/repositories/user'
-
-const server = new ApolloServer({
-  typeDefs: gql`
-    ${typeDefs}
-  `,
-  resolvers
-});
-
-const { query, mutate } = createTestClient(server);
 
 afterEach(() => {
   jest.restoreAllMocks()
@@ -20,73 +7,38 @@ afterEach(() => {
 
 describe('Query', () => {
   describe('users', () => {
-    const message = gql`
-      query ($name: String) {
-        users(name: $name) {
-          id
-          name
-        }
-      }
-    `
-    it('find users', async () => {
-      const variables = { name: 'user1' }
-      const expectedResult = [{
-        id: '1234',
-        name: 'user1'
-      }]
-      const findMock = jest.fn(() => [{ id: '1234', name: 'user1' }])
+    it('find user', async () => {
+      const parent = {}
+      const args = { name: 'user1' }
+
+      const expectedResult = [{ id: '1', name: 'user1' }]
+      const findMock = jest.fn(() => expectedResult)
       jest
         .spyOn(UserRepository.prototype, 'find')
         .mockImplementationOnce(findMock)
+      const result = await UserResolver.Query.users(parent, args)
 
-      const {
-        data: {
-          users
-        }
-      } = await query({
-        query: message,
-        variables,
-      })
-
-      expect(findMock).toHaveBeenCalledWith({ name: variables.name })
-      expect(users).toEqual(expectedResult)
+      expect(findMock).toHaveBeenCalledWith({ name: 'user1' })
+      expect(result).toBe(expectedResult)
     })
   })
 })
 
 describe('Mutation', () => {
   describe('createUser', () => {
-    const message = gql`
-      mutation ($name: String!) {
-        createUser(name: $name) {
-          id
-          name
-        }
-      }
-    `
-
     it('create user', async () => {
-      const variables = { name: 'user1' }
-      const expectedResult = {
-        id: '1234',
-        name: 'user1'
-      }
-      const createMock = jest.fn(() => ({ id: '1234', name: 'user1' }))
+      const parent = {}
+      const args = { name: 'user1' }
+
+      const expectedResult = { id: '1', name: 'user1' }
+      const createMock = jest.fn(() => expectedResult)
       jest
         .spyOn(UserRepository.prototype, 'create')
         .mockImplementationOnce(createMock)
+      const result = await UserResolver.Mutation.createUser(parent, args)
 
-      const {
-        data: {
-          createUser
-        }
-      } = await mutate({
-        mutation: message,
-        variables,
-      })
-
-      expect(createMock).toHaveBeenCalledWith(variables.name)
-      expect(createUser).toEqual(expectedResult)
+      expect(createMock).toHaveBeenCalledWith('user1')
+      expect(result).toBe(expectedResult)
     })
   })
 })
