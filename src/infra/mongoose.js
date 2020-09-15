d8fec483c8bba13da5e008f44136edc407028dbc
natapas <natapas.lue@gmail.com>
2020-09-15 17:43:45 +0700

    Remove db url

--- src/infra/mongoose.js
+++ src/infra/mongoose.js
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose'
 
-process.env.DB_URL = 'mongodb://localhost:27017/test'
-
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
